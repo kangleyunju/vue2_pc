00a48a72b46a77c929515e8fc2da6a3f2673b0e6
kangle <249042680@qq.com>
2022-11-13 22:36:01 +0800

    完善滑动验证码

--- src/views/tool/page/verify.vue
+++ src/views/tool/page/verify.vue
@@ -1,77 +1,30 @@
 <template>
-	<div class="verifyContainer">
+	<div class="verifyContainer noCopy">
 		<el-tabs v-model="tabName" type="border-card" @tab-click="changeTab">
 			<el-tab-pane label="基础滑块" name="1">
-				<dragVerify
-				v-if="tabName=='1'"
-					ref="dragVerify"
-					:isPassing.sync="isPassing"
-					text="请按住滑块拖动"
-					successText="验证通过"
-					handlerIcon="el-icon-d-arrow-right"
-					successIcon="el-icon-circle-check"
-					@passcallback="passcallback(1)">
-				</dragVerify>
+				<dragVerify ref="dragVerify" :isPassing.sync="isPassing" @passSuccess="success" />
 			</el-tab-pane>
 			<el-tab-pane label="图片滑块" name="2">
-				<dragVerifyImg
-					v-if="tabName=='2'"
-					ref="dragVerifyImg"
-					:imgsrc="img"
-					:isPassing.sync="isPassing2"
-					:showRefresh="true"
-					text="请按住滑块拖动"
-					successText="验证通过"
-					handlerIcon="el-icon-d-arrow-right"
-					successIcon="el-icon-circle-check"
-					refreshIcon="el-icon-refresh-right"
-					@refresh="refresh(1)"
-					@passcallback="passcallback(2)">
-				</dragVerifyImg>
+				<dragVerifyImg ref="dragVerifyImg" :isPassing.sync="isPassing2" :imgsrc="img" @refresh="reset" @passSuccess="success" />
 			</el-tab-pane>
 			<el-tab-pane label="拼图滑块" name="3">
-				<dragVerifyChip
-					ref="dragVerifyChip"
-					:imgsrc="img2"
-					:isPassing.sync="isPassing3"
-					:showRefresh="true"
-					:barWidth="40"
-					text="请按住滑块拖动"
-					successText="验证通过"
-					handlerIcon="el-icon-d-arrow-right"
-					successIcon="el-icon-circle-check"
-					refreshIcon="el-icon-refresh-right"
-					@refresh="refresh(2)"
-					@passcallback="passcallback(3)">
-				</dragVerifyChip>
+				<dragVerifyChip ref="dragVerifyChip" :isPassing.sync="isPassing3" :imgsrc="img2" @refresh="reset" @passSuccess="success" />
 			</el-tab-pane>
 			<el-tab-pane label="旋转图片" name="4">
-				<dragVerifyRotate
-					ref="dragVerifyRotate"
-					:imgsrc="img3"
-					:isPassing.sync="isPassing4"
-					:showTips="true"
-					text="请按住滑块拖动"
-					successText="验证通过"
-					handlerIcon="el-icon-d-arrow-right"
-					successIcon="el-icon-circle-check"
-					refreshIcon="el-icon-refresh-right"
-					@refresh="refresh(3)"
-					@passcallback="passcallback(4)">
-				</dragVerifyRotate>
+				<dragVerifyRotate ref="dragVerifyRotate" :isPassing.sync="isPassing4" :imgsrc="img3" @refresh="reset" @passSuccess="success" />
 			</el-tab-pane>
 		</el-tabs>
 	</div>
 </template>
 <script>
 	// 基本滑块验证组件
-	import dragVerify from "@/components/dragVerify";
+	import dragVerify from "@/components/verify/dragVerify";
 	// 图片滑块组件
-	import dragVerifyImg from "@/components/dragVerifyImg";
+	import dragVerifyImg from "@/components/verify/dragVerifyImg";
 	// 拼图滑块组件
-	import dragVerifyChip from "@/components/dragVerifyImgChip";
+	import dragVerifyChip from "@/components/verify/dragVerifyImgChip";
 	// 旋转图片滑块组件
-	import dragVerifyRotate from "@/components/dragVerifyImgRotate";
+	import dragVerifyRotate from "@/components/verify/dragVerifyImgRotate";
 	export default {
 		components: {
 			dragVerify,
@@ -83,72 +36,79 @@
 			return {
 				img: '',
 				img2: '',
-				img3:'',
+				img3: '',
 				imgList: [
 					require('@/assets/home/face.png'),
-					require('@/assets/home/face2.png')
+					require('@/assets/home/face2.png'),
+					require('@/assets/home/face3.png')
 				],
-				isPassing:false,
-				isPassing2:false,
-				isPassing3:false,
-				isPassing4:false,
-				tabName:'1'
+				isPassing: false,
+				isPassing2: false,
+				isPassing3: false,
+				isPassing4: false,
+				tabName: '1'
 			}
 		},
 		methods: {
-			changeTab(e){
-				this.tabName=e.name
-				if (this.tabName == 1) {
-					this.$refs.dragVerify.reset()
-				} else if (this.tabName == 2) {
-					console.log('9999')
-					this.$refs.dragVerifyImg.reset()
-				} else if(this.tabName==3){
-					this.$refs.dragVerifyChip.reset()
-				}else{
-					this.$refs.dragVerifyRotate.reset()
-				}
+			changeTab(e) {
+				this.tabName = e.name
+				this.reset()
 			},
-			refresh(e) {
-				if(e==1){
+			reset() {
+				if (this.tabName == 1) {
+					this.isPassing = false
+					this.$refs.dragVerify.reset()
+				}
+				if (this.tabName == 2) {
+					this.isPassing2 = false
 					this.img = this.getImg()
-				}else{
-					this.img2= this.getImg()
+					this.$refs.dragVerifyImg.reset()
+				}
+				if (this.tabName == 3) {
+					this.isPassing3 = false
+					this.img2 = this.getImg()
+					this.$refs.dragVerifyChip.reset()
+				}
+				if (this.tabName == 4) {
+					this.isPassing4 = false
+					this.img3 = this.getImg()
+					this.$refs.dragVerifyRotate.reset()
 				}
 			},
 			getImg() {
 				return this.imgList[Math.floor(Math.random(0, 1) * this.imgList.length)]
 			},
-			//验证通过
-			passcallback(e) {
+			//失败回调
+			fail() {
+				setTimeout(() => {
+					this.reset()
+				}, 500)
+			},
+			//成功回调
+			success() {
 				this.$message.success('验证通过')
 				setTimeout(() => {
-					if (e == 1) {
-						this.$refs.dragVerify.reset()
-					} else if (e == 2) {
-						this.$refs.dragVerifyImg.reset()
-					} else if(e==3){
-						this.$refs.dragVerifyChip.reset()
-					}else{
-						this.$refs.dragVerifyRotate.reset()
-					}
+					this.reset()
 					this.$message.closeAll()
-				}, 2000)
+				}, 1500)
 			}
 		},
 		mounted() {
-			this.img = this.getImg()
-			this.img2 = this.getImg()
-			this.img3 = this.getImg()
-			this.$refs.dragVerify?.reset()
+			this.reset()
 		}
 	}
 </script>
 <style lang="scss">
 	.verifyContainer {
 		margin: 16px;
-		.el-tabs{
-			height: 450px;
+		.el-tabs {
+			height: 380px;
+			.el-tab-pane:first-child {
+				padding-top: 50px;
+			}
+			.el-tabs__content {
+				padding: 24px;
+			}
 		}
 	}
 </style>
